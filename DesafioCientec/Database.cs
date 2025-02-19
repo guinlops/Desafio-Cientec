6283c2956c0e2d812741fb4092ec8b2479c27162
Guilherme Nunes Lopes <gui.nunes253@gmail.com>
2025-02-19 20:13:48 -0300

    Estruturação da classe Database
    Database em uma classe separada.
    myConnection como static para facilitar seu uso.
    Database será usada a partir de composição.

---
+++ DesafioCientec/Database.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+namespace DesafioCientec
+{
+    internal class Database
+    {
+        private static SQLiteConnection? myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source= database.sqlite3");
+
+            //Criar pela primeira vez
+
+            if (!File.Exists("./database.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("database.sqlite3");
+                System.Console.WriteLine("Database file created");
+                //Create Tables;
+            }
+        }
+
+
+        public void OpenConnection()
+        {
+            if (myConnection?.State != System.Data.ConnectionState.Open)
+            {
+                //Console.WriteLine("Database aberto!");
+                myConnection?.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+
+            if (myConnection?.State != System.Data.ConnectionState.Closed)
+            {
+
+                myConnection?.Close();
+
+            }
+        }
+
+        public static SQLiteConnection GetConnection()
+        {
+
+            if (myConnection == null)
+            {
+                throw new InvalidOperationException("A conexão com o banco de dados não foi inicializada.");
+            }
+
+            return myConnection;
+        }
+    }
+}
+
