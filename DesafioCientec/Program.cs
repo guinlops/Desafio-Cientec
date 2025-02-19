0dfb2701ef66e474a525dfdfc74d7bcd21ce214f
Guilherme Nunes Lopes <gui.nunes253@gmail.com>
2025-02-19 20:33:19 -0300

    Update Main

--- DesafioCientec/Program.cs
+++ DesafioCientec/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            App app = new App();
+            app.init();
         }
     }
 }
